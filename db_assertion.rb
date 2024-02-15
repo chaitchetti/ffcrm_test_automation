af402f33629d98d95982381dcd998c9ee69d884d
chaitchetti <114266571+chaitchetti@users.noreply.github.com>
2024-02-14 21:23:55 -0800

    Updated assertion code

--- db_assertion.rb
+++ db_assertion.rb
@@ -1,19 +1,19 @@
 require 'mysql2'
 require 'minitest/autorun'
 
-def assert_record_exists(client, table_name, condition_column, condition_value)
+def assert_record_not_exists(client, table_name, condition_column, condition_value)
   query = "SELECT * FROM #{table_name} WHERE #{condition_column} = '#{condition_value}' LIMIT 1"
 
   result = client.query(query)
 
   # Use assert to check if the number of rows is greater than 0
-  assert(result.count == 0, "Record not found for #{condition_column}=#{condition_value}")
+  assert(result.count > 0, "Record found for #{condition_column}=#{condition_value}")
 end
 
 class TestRecordExistence < Minitest::Test
   def setup
     @client = Mysql2::Client.new(
-      host: "34.218.237.182",
+      host: "54.201.36.55",
       username: "test",
       password: "test",
       database: "fat_free_crm_development"
@@ -24,11 +24,11 @@ class TestRecordExistence < Minitest::Test
     @client.close if @client
   end
 
-  def test_record_exists
+  def test_record_not_exists_after_deletion
     table_name = "accounts"
     condition_column = "name"
     condition_value = "chait"
 
-    assert_record_exists(@client, table_name, condition_column, condition_value)
+    assert_record_not_exists(@client, table_name, condition_column, condition_value)
   end
 end
